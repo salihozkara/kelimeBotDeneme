5e5b69f4357a41ff9fd8607adfa20e2f46c94fb6
salih191 <58659931+salih191@users.noreply.github.com>
2020-10-26 10:46:14 +0300

    Create Form1.cs

---
+++ Yeni klasör/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TODO1
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool a;
+        public void Key()
+        {
+            a = true;
+            double bekle = 1000 *Convert.ToDouble(numericUpDown2.Text);
+            while (a)
+            {
+                Thread.Sleep((int)bekle);
+                SendKeys.SendWait(textBox1.Text+"{Enter}");
+            }
+                          
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(new ThreadStart(Key));
+            thread.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = false;
+            //thread.Abort();
+        }
+    }
+}
